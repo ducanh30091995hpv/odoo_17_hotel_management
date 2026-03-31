b60c9e593e7f2de75a2ec90e5ef19683ffaf9bed
ducanh30091995hpv <maiducanh1995hp@gmail.com>
2026-03-31 15:25:49 +0700

    2222222222

--- web_admin_aurora/static/src/webclient/webclient_patch.js
+++
@@ -1,9 +0,0 @@
-/** @odoo-module **/
-
-import { WebClient } from "@web/webclient/webclient";
-import { SideBar } from "../sidebar/sidebar";
-import { patch } from "@web/core/utils/patch";
-
-patch(WebClient.components, {
-    SideBar,
-});
